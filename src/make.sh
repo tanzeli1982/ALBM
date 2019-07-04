e2cd67972ee625bbef6922419f0f287172d81500
Zeli Tan <tanzeli1982@gmail.com>
2019-07-03 22:54:43 -0700

    1) add tw observations; 2) add bathymetry; 3) change Makefile

--- src/make.sh
+++ src/make.sh
@@ -5,11 +5,23 @@
 ##------------
 ##
 
-module unload netcdf/4.3.2 
+module unload netcdf
 
-export NETCDF_HOME=/people/tanz151/packages/Pnetcdf_openmpi_1.8.3
-export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
-module load intel/15.0.1 openmpi/1.8.3
+mach=$( echo $HOSTNAME | cut -f 1 -d . -d 0 )
+
+if [ $mach = 'constance' ]; then
+
+   export NETCDF_HOME=/people/tanz151/packages/Pnetcdf_openmpi_1.8.3
+   export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
+   module load intel/15.0.1 openmpi/1.8.3
+
+elif [ $mach = 'compy' ]; then
+
+   export NETCDF_HOME=/people/tanz151/packages/Pnetcdf_mvapich_2.3.1
+   export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
+   module load intel/19.0.3 mvapich2/2.3.1
+
+fi
 
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
