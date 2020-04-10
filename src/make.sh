dd714e671e0a6d7f071e87403914d88c3a040ae1
Zeli Tan <tanzeli1982@gmail.com>
2020-04-09 20:20:56 -0700

    gmake -> make for all platforms

--- src/make.sh
+++ src/make.sh
@@ -10,13 +10,13 @@ export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
 
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
-   gmake OPT=1 -j8
+   make OPT=1 -j8
 elif [ $arg = 'trace' ]; then
-   gmake TRACE=1 -j8
+   make TRACE=1 -j8
 elif [ $arg = 'clean' ]; then
-   gmake $arg -j8
+   make $arg -j8
 elif [ $arg = 'debug' ]; then
-   gmake DEBUG=1 TRACE=1 -j8
+   make DEBUG=1 TRACE=1 -j8
 else
    echo "Wrong Argument: $1!!!"
 fi
