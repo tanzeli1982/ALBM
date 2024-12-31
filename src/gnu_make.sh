7bf6999102b481a79ee6f6b65458e9272b63658f
Zeli Tan <tanzeli1982@gmail.com>
2024-12-30 22:11:57 -0800

    minor changes to compiler settings

--- src/gnu_make.sh
+++ src/gnu_make.sh
@@ -11,6 +11,8 @@ export NETCDF_HOME=/qfs/people/tanz151/packages/Pnetcdf-1.14.0_gnu
 export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
 module load gcc/4.8.5 mvapich2/2.3.1
 
+export FC=gfortran
+
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
    gmake -j8
--- src/make.sh
+++ src/make.sh
@@ -11,6 +11,8 @@ export NETCDF_HOME=/people/tanz151/packages/Pnetcdf_mvapich_2.3.1
 export LD_LIBRARY_PATH=$NETCDF_HOME/lib:$LD_LIBRARY_PATH
 module load intel/19.0.3 mvapich2/2.3.1
 
+export FC=ifort
+
 arg=$( echo $1 | tr '[:upper:]' '[:lower:]' )
 if [ -z "$arg" ]; then
    gmake -j8
